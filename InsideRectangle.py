28887a0aeb7bbd09b4957e38281b7db737199b5a
Neal L <rlawton.cs@gmail.com>
2024-04-14 18:39:39 -0700

    Initial commit.

---
+++ InsideRectangle.py
@@ -0,0 +1,54 @@
+# PROGRAMMER: Ryan Lawton
+
+# IMPORT STATEMENTS
+
+
+
+
+
+class InsideRectangle:
+
+# CONSTRUCTOR
+
+    def __init__(self, width, height, offset, x_centroid, y_centroid):
+        self.__width = width
+        self.__height = height
+        self.__offset = offset
+        self.__x_centroid = x_centroid
+        self.__y_centroid = y_centroid
+
+# INSTANCE METHODS
+    
+    def get_width(self):
+        pass
+    
+    def get_height(self):
+        pass
+    
+    def get_offset(self):
+        pass
+    
+    def get_x_centroid(self):
+        pass
+    
+    def get_y_centroid(self):
+        pass
+    
+    def rectangle_points_clockwise(self):
+        pass
+    
+    def rectangle_points_counterclockwise(self):
+        pass
+    
+    def dashed_rectangle_points_clockwise(self):
+        pass
+    
+    def dashed_rectangle_points_counterclockwise(self):
+        pass
+    
+    def dotted_rectangle_points_clockwise(self):
+        pass
+    
+    def dotted_rectangle_points_counterclockwise(self):
+        pass
+    
